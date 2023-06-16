de2eb3ffd07ec23e1a575a11f7b39efd2640487e
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-06-16 19:34:49 +0800

    Написал (спёр из интернета) способ ввести и отобразить введённые строки.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Control
+{
+    class program
+    {
+        static void Main()
+        {
+            string[] Massive;
+            string[] Massive2;
+            int count;
+            string message;
+            Console.WriteLine("Введите строки; оставьте строку пустой для завершения ввода. ");
+            count = 0;
+            Massive = new string[count];
+            
+            do
+            {
+                message = Console.ReadLine();
+                if (message != "")
+                {
+                    count++;
+                    Massive2 = new string[count];
+                    for (int i = 0; i < Massive2.Length - 1; i++)
+                        Massive2[i] = Massive[i];
+                    Massive2[count - 1] = message;
+                    Massive = Massive2;
+                }
+            }
+            while (message != "");
+            for (int i = 0; i < Massive.Length; i++)
+                Console.WriteLine(Massive[i]);
+            
+            
+        }
+    }
+
+}
